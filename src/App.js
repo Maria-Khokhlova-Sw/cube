849c6cd59172c6570a5c81cf4247c7334b064511
Maria <mariahohlov9955@gmail.com>
2025-09-04 11:18:34 +0600

    cube

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.jsx
 import React from "react";
 import "./cube.css";
 
@@ -7,19 +6,17 @@ export default function App() {
         <div className="page">
             <div className="scene">
                 <div className="cube" aria-label="Wireframe cube (12 edges)">
-                    {/* 4 рёбра вдоль оси X (y=±h, z=±h) */}
+
                     <div className="edge x x1" />
                     <div className="edge x x2" />
                     <div className="edge x x3" />
                     <div className="edge x x4" />
 
-                    {/* 4 рёбра вдоль оси Y (x=±h, z=±h) */}
                     <div className="edge y y1" />
                     <div className="edge y y2" />
                     <div className="edge y y3" />
                     <div className="edge y y4" />
 
-                    {/* 4 рёбра вдоль оси Z (x=±h, y=±h) */}
                     <div className="edge z z1" />
                     <div className="edge z z2" />
                     <div className="edge z z3" />
